2e2983b4bee6fc93b02bc9a9108bad2b379ca2c9
ElJamonero <94261785+ElJamonero@users.noreply.github.com>
2024-10-15 18:53:08 +0200

    Update Diferentes parkings.sql

--- Diferentes parkings.sql
+++ Diferentes parkings.sql
@@ -1,10 +1,10 @@
-insert into parkings(nombre, ubicacion, plazas_disponibles, plazas_restantes, total_plazas) values
-('Primero', 'Avinguda Andorra', '50', '50', '50'),
-('Segundo', 'Rambla Nova', '50','50', '50'),
-('Tercero', 'Rambla Vella', '50','50', '50'),
-('Cuarto', 'Plaça dels carrros', '50','50', '50'),
-('Quinto', 'Port', '50','50', '50'),
-('Sexto', 'Serrallo', '50','50', '50'),
-('Séptimo', 'Casino', '50','50', '50'),
-('Octavo', 'Ajuntament', '50','50', '50')
+insert into parkings(nombre, ubicacion, plazas_disponibles, total_plazas) values
+('Primero', 'Avinguda Andorra', '50', '50'),
+('Segundo', 'Rambla Nova', '50','50'),
+('Tercero', 'Rambla Vella', '50','50'),
+('Cuarto', 'Plaça dels carrros', '50','50'),
+('Quinto', 'Port', '50','50'),
+('Sexto', 'Serrallo', '50','50'),
+('Séptimo', 'Casino', '50','50'),
+('Octavo', 'Ajuntament', '50','50')
 ;
